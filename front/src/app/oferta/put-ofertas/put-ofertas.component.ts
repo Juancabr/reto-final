3a6b92efc179458f7d28b18478d962c1f6107758
vijodar <vicentejodar97@gmail.com>
2019-10-25 13:03:17 +0200

    Routing, Put Ofertas

--- front/src/app/oferta/put-ofertas/put-ofertas.component.ts
+++ front/src/app/oferta/put-ofertas/put-ofertas.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Oferta } from '../oferta';
+import { OfertaService } from '../oferta.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-put-ofertas',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PutOfertasComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  oferta: Oferta;
+  errorMessage: string;
+
+  constructor(private ofertaService: OfertaService, private router: Router, private route: ActivatedRoute) {
+    this.oferta = <Oferta>{};
+  }
 
   ngOnInit() {
+    const ownerId = this.route.snapshot.params['id'];
+    this.ofertaService.getOfertaById(ownerId).subscribe(
+      oferta => this.oferta = oferta,
+      error => this.errorMessage = <any>error);
+  }
+
+  onSubmit(oferta: Oferta) {
+    oferta.id = 0;
+    console.log(oferta);
+
+    this.ofertaService.addOferta(oferta).subscribe(
+      new_oferta => {
+        this.oferta = new_oferta;
+        this.gotoOfertasList();
+      },
+      error => this.errorMessage = <any>error
+    );
+  }
+
+  gotoOfertasList() {
+    this.router.navigate(['/ofertas']);
   }
 
 }
